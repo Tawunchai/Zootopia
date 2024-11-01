22eac58fc6c120723df926d322ce091752274e7e
Tawunchai <161191262+Tawunchai@users.noreply.github.com>
2024-11-01 21:05:36 +0700

    add

---
+++ frontend/src/feature/averagestar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const averagestar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default averagestar
---
+++ frontend/src/feature/like.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const like = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default like
