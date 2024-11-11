f1a25c155edf3da37bf522726af1b51200ac2b75
Tawunchai <161191262+Tawunchai@users.noreply.github.com>
2024-11-11 22:27:32 +0700

    add

---
+++ frontend/src/page/Staff/report_repair/repair.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const repair = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default repair
---
+++ frontend/src/page/Zookeeper/habitat/habitat.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const habitat = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default habitat
